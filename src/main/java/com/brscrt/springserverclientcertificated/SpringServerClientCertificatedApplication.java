9e8a8e8dd7b4a10d72e8bdf7d7738452a2e93d34
Barış Cirit <brscr@gmail.com>
2019-09-28 23:32:06 +0300

    first commit

---
+++ src/main/java/com/brscrt/springserverclientcertificated/SpringServerClientCertificatedApplication.java
@@ -0,0 +1,13 @@
+package com.brscrt.springserverclientcertificated;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringServerClientCertificatedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringServerClientCertificatedApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/brscrt/springserverclientcertificated/controller/CustomerController.java
@@ -0,0 +1,22 @@
+package com.brscrt.springserverclientcertificated.controller;
+
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.brscrt.springserverclientcertificated.pojo.Customer;
+
+
+
+@RestController
+@RequestMapping("/customer")
+public class CustomerController {
+
+    @GetMapping("/{id}")
+    @Secured("ROLE_USER")
+    public Customer GetCustomer(@PathVariable Long id) {
+        return new Customer(id, "Customer" + id);
+    }
+}
